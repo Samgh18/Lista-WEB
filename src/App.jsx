55a7a86aa064f3195b08eab1f87b737bbcd9feb6
Samuel Hernanes <samuel.hernanes@facens.br>
2025-06-23 15:35:16 -0300

    Exercicio 1 lista

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Bem-vindo ao Meu Projeto Frontend!</h1>
+      <p>Este é um projeto React simples para teste de deploy.</p>
+    </div>
+  );
+}
+
+export default App;
